0c138e123383c7dfab90d5314134297fa6d9e2d9
Fatih Ateş <fth20043@gmail.com>
2026-07-22 12:12:33 +0300

    Set up the service.

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Car {
+  arac_ID: number;
+  marka_Model: string;
+  plaka: string;
+  gunluk_Fiyat: number;
+  durum: string
+}
 
 @Injectable({
   providedIn: 'root',
 })
-export class Api {
+export class ApiService {
 
+  private baseUrl = 'https://localhost:7101/api';
+
+  constructor(private http: HttpClient) { }
+
+  getAllCars(): Observable<Car[]> {
+    return this.http.get<Car[]>('${this.baseUrl}/cars');
+  }
 }
